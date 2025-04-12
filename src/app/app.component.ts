b94f99c15af2de64c04596bc13a4bf364f3dcb83
Sunny Tsai <pl.sunnytsai@gmail.com>
2025-04-12 05:41:31 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ CommonModule,MatToolbarModule,MatSidenav,MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
   title = 'chat-demo';
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,40 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration } from '@angular/platform-browser';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(), 
+    provideAnimationsAsync(),     
+    provideAnimations(), // Required for Material    
+    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(MatDialogModule),
+    importProvidersFrom(MatSnackBarModule),
+    importProvidersFrom(MatTooltipModule),
+    importProvidersFrom(MatButtonModule),
+    importProvidersFrom(MatCardModule),
+    importProvidersFrom(MatSidenavModule),
+    importProvidersFrom(MatToolbarModule),
+    importProvidersFrom(MatProgressSpinnerModule), provideAnimationsAsync()
+  ]
 };
